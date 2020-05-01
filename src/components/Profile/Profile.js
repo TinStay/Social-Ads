926e320aac9c5af45b7bb33d53cc96e7f36fbc00
Martin Staykov <marti.staykov@gmail.com>
2020-05-01 17:50:51 +0300

    Added a profile form

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import app from "../../base";
-import accPic from '../../assets/accPic.png';
+import accPic from '../../assets/girlPic.jpg';
 
 
 const Profile = props =>{
@@ -10,14 +10,61 @@ const Profile = props =>{
             <div className="gradient-bgc">
                 <div className="profile-page">
                     <div className="profile-card">
-                        <div className="row mt-4 border-bottom">
-                            <div className="col-md-3 text-center">
-                                <img className="profile-pic" src={accPic} alt="profile picture"/>
-                            </div>
-                            <div className="col-md-9">
-                                <h1>My Profile</h1>
-                            </div>
+                    <img className="profile-pic" src={accPic} alt="profile picture"/>
+                    <div className="profile-info">
+                        <h1 className="profile-heading">My Profile</h1>
+                        <div className="profile-form">
+                            <form>
+                                <div class="form-row">
+                                    <div class="form-group col-md-6">
+                                    <label for="inputEmail4">Email</label>
+                                    <input type="email" class="form-control" id="inputEmail4" placeholder="Email"/>
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                    <label for="inputPassword4">Password</label>
+                                    <input type="password" class="form-control" id="inputPassword4" placeholder="Password"/>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label for="inputAddress">Address</label>
+                                    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St"/>
+                                </div>
+                                <div class="form-group">
+                                    <label for="inputAddress2">Address 2</label>
+                                    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor"/>
+                                </div>
+                                <div class="form-row">
+                                    <div class="form-group col-md-6">
+                                    <label for="inputCity">City</label>
+                                    <input type="text" class="form-control" id="inputCity"/>
+                                    </div>
+                                    <div class="form-group col-md-4">
+                                    <label for="inputState">State</label>
+                                    <select id="inputState" class="form-control">
+                                        <option selected>Choose...</option>
+                                        <option>...</option>
+                                    </select>
+                                    </div>
+                                    <div class="form-group col-md-2">
+                                    <label for="inputZip">Zip</label>
+                                    <input type="text" class="form-control" id="inputZip"/>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" id="gridCheck"/>
+                                    <label class="form-check-label" for="gridCheck">
+                                        Check me out
+                                    </label>
+                                    </div>
+                                </div>
+                                <button type="submit" class="btn btn-primary">Sign in</button>
+                            </form>
                         </div>
+                    </div>
+                                
+
+                        
                     </div>
                     
                 </div>
