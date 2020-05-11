19b9c4c8d0fd8168f7e82a1aa0f767de4d7a5827
Martin Staykov <marti.staykov@gmail.com>
2020-05-11 14:50:21 +0300

    Changed class names

--- src/containers/AdManager/AdManager.js
+++ src/containers/AdManager/AdManager.js
@@ -1,7 +1,11 @@
-import React, { useContext, PureComponent } from "react";
+import React, {  PureComponent } from "react";
 import { Route, Redirect } from "react-router-dom";
 import { AuthContext } from "../../components/Auth/Auth";
-import { render } from "@testing-library/react";
+import { Form } from 'react-bootstrap';
+import facebookAds from '../../assets/facebookAds.png'
+import instagramAds from '../../assets/instagramAds.png'
+import googleAds from '../../assets/adsGoogle.png'
+import twitterAds from '../../assets/twitterAds.png'
 
 class AdManager extends PureComponent{
 //   const {currentUser} = this.context;
@@ -15,6 +19,7 @@ class AdManager extends PureComponent{
     showForm = () => {
         this.setState({ showForm: true})
     }
+
 
   render(){
       let adSection = (
@@ -30,10 +35,74 @@ class AdManager extends PureComponent{
             </div>
           </div>
       ); 
+
+      
     if(this.state.showForm){
         adSection =(
-            <div>
-                <h1>Start an ad campaign</h1>
+            <div className=" manager-ad-form-row">
+                {/* <div className="d-block mb-5">
+                    <h1 className="text-center purple border-bottom pb-2 mb-4">Start an ad campaign</h1>
+                </div> */}
+                <div className="ad-container">
+                    <Form className="first-form">
+
+                        <Form.Group className="first-form-group text-center" controlId="formGroupEmail">
+                            <h3 className="first-form-label">Choose a name for your campaign</h3>
+                            <Form.Control className="first-form-input-name " type="text" size="lg" placeholder="Enter name" />
+                        </Form.Group>
+
+                        <h3 className="border-bottom first-form-label">Choose social media platforms</h3>
+                        <div className="social-media-selection d-md-flex justify-content-between">
+                            <div className="social-media-box">
+                                <Form.Check
+                                    custom
+                                    inline
+                                    label=""
+                                    type="checkbox"
+                                    id={`custom-inline-checkbox-facebook`}
+                                />
+                                <img className="img-slim" src={facebookAds} alt="Facebook ads"/>
+                            </div>
+                            <div className="social-media-box">
+                                <Form.Check
+                                    custom
+                                    inline
+                                    label=""
+                                    type="checkbox"
+                                    id={`custom-inline-checkbox-insta`}
+                                />
+                                <img className="img-slim" src={instagramAds} alt="Instagram Ads"/>
+                            </div>
+                            <div className="social-media-box">
+                                <Form.Check
+                                    custom
+                                    inline
+                                    label=""
+                                    type="checkbox"
+                                    id={`custom-inline-checkbox-google`}
+                                />
+                                 <img className="img-box" src={googleAds} alt="Google Ads"/>
+                            </div>
+                            <div className="social-media-box">
+                                <Form.Check
+                                    custom
+                                    inline
+                                    label=""
+                                    type="checkbox"
+                                    id={`custom-inline-checkbox-twitter`}
+                                />
+                                <img className="img-box"src={twitterAds} alt="Twitter Ads"/>
+                            </div>
+                            
+                        </div>
+
+                        <div className="d-flex justify-content-end">
+                            {/* <button className="btn btn-cancel">Cancel</button> */}
+                            <button className="btn btn-next">Next</button>
+                        </div>
+                    </Form>
+
+                </div>
             </div>
         )
     }
