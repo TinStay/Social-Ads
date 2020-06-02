761ff59af31bbee0bc1f085cce3ab67393a0a0ef
Martin Staykov <marti.staykov@gmail.com>
2020-06-02 13:37:01 +0300

    Fixed decimal budget numbers

--- src/components/AdManager/CreateAd/BudgetAndDate/BudgetAndDate.js
+++ src/components/AdManager/CreateAd/BudgetAndDate/BudgetAndDate.js
@@ -1,7 +1,6 @@
 import React,{ useState, useEffect } from 'react';
-import DatePicker from "react-datepicker";
+import DatePicker,{addDays} from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import NumberPicker from 'react-widgets/lib/NumberPicker'
 import  { Form  } from 'react-bootstrap'
 
 // import Globalize from 'globalize';
@@ -19,21 +18,30 @@ const BudgetAndDate = (props) => {
 
     const setDailyAndLifetimeBudget = e => {
         if(e.target.name === "daily"){
-            setDailyBudget((e.target.value).toFixed(2))
-            setLifetimeBudget((e.target.value * 30).toFixed(2))
+            const adCost = parseInt(e.target.value).toFixed(2)
+            setDailyBudget((adCost))
+            setLifetimeBudget((adCost * 30))
         }else{
-            setDailyBudget((e.target.value / 30).toFixed(2))
-            setLifetimeBudget((e.target.value).toFixed(2))
+            const adCost = parseInt(e.target.value).toFixed(2)
+            setDailyBudget((adCost / 30).toFixed(2))
+            setLifetimeBudget((adCost))
         }
     }
 
 
-    // Schedule 
+    // Schedule type
     const [asapSchedule, setAsapSchedule] = useState(true);
     const [customSchedule, setCustomSchedule] = useState(false);
 
-    const [startingDate, setStartingDate] = useState();
-    const [endingDate, setEndingDate] = useState();
+    // Schedule dates
+    const endDateMin = new Date();
+    endDateMin.setDate(endDateMin.getDate() + 29);
+
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(endDateMin);
+
+    
+    console.log("startdate",endDate.getDate() - startDate.getDate())
 
 
     const changeToDaily = (e) => {
@@ -64,63 +72,12 @@ const BudgetAndDate = (props) => {
         }
     }
     
-    
+  
+
     return(
         <div className="add-form-group">
             <h3 className="border-bottom add-form-label">Choose your budget and starting date</h3>
             <form className="budget-form row">
-
-            <div className="budget-form-budget col-md-5">
-               <h3 className="budget-form-label font-color">Budget</h3>
-                    <div className="daily-budget-field d-flex ">
-                        {/* <div className=""> */}
-                            <Form.Check
-                                custom
-                                block
-                                label="Daily"
-                                type="radio"
-                                id={"daily-budget-radio"}
-                                className="radio-budget"
-                                name="dailyBudget"
-                                checked={isDailyBudget}
-                                onChange={(e) => changeToDaily(e)}
-                            />
-                        {/* </div> */}
-                       { isDailyBudget ?  
-                       <div className="input-group mx-3">
-                            <div className="input-group-prepend">
-                                <span className="input-group-text" id="basic-addon1">USD</span>
-                            </div>
-                            <input type="number" name="daily" value={dailyBudget} onChange={(e) => setDailyAndLifetimeBudget(e)} className="form-control" placeholder="Daily budget" aria-label="budget" aria-describedby="basic-addon1"/>
-                        </div> : null }
-                    </div>
-                    <div className="lifetime-budget-field d-flex">
-                        <div className="">
-                            <Form.Check
-                                custom
-                                block
-                                label="Lifetime"
-                                type="radio"
-                                id={`lifetime-budget-radio`}
-                                className="radio-budget"
-                                name="lifetimeBudget"
-                                checked={isLifetimeBudget}
-                                onChange={(e) => changeToLifetime(e)}
-                            />
-                        </div>
-                        { isLifetimeBudget ?  
-                        <div className="input-group ml-3">
-                            <div className="input-group-prepend">
-                                <span className="input-group-text" id="basic-addon1">USD</span>
-                            </div>
-                            <input type="number" name="lifetime" value={lifetimeBudget} onChange={(e) => setDailyAndLifetimeBudget(e)} className="form-control" placeholder="Lifetime budget" aria-label="budget" aria-describedby="basic-addon1"/>
-                        </div> : null }
-                    </div>
-                    <div className="spending">
-                        <p className="gray mt-3">You will spend no more than {isLifetimeBudget ? lifetimeBudget  : dailyBudget*30 }$/month.</p>
-                    </div>
-                 
-            </div>
 
             <div className="budget-form-schedule col-md-5 offset-md-1">
                 <h3 className="budget-form-label font-color ">Schedule</h3>
@@ -155,19 +112,27 @@ const BudgetAndDate = (props) => {
                         <div className="">
                             <Form.Group>
                             <div className="start-date d-flex">
-                                <label className="mr-2 my-auto">Starting date:</label>
+                                <label className="mr-2 my-auto">Start date:</label>
                                 <DatePicker
-                                selected={startingDate}
-                                onChange={(date) => setStartingDate(date)}
+                                selected={startDate}
+                                onChange={(date) => setStartDate(date)}
+                                minDate={new Date()}
+                                showTimeSelect
+                                dateFormat="dd/MM/yyyy h:mm aa"
                                 className="date-picker"
+                                placeholderText="Click to select a date"
                                 />
                             </div>
                             <div className="end-date d-flex">
-                                <label className="mr-2 my-auto">Ending date:</label>
+                                <label className="mr-2 my-auto">End date:</label>
                                 <DatePicker
-                                selected={endingDate}
-                                onChange={(date) => setEndingDate(date)}
+                                selected={endDate}
+                                onChange={(date) => setEndDate(date)}
+                                minDate={endDateMin}
+                                showTimeSelect
+                                dateFormat="dd/MM/yyyy h:mm aa"
                                 className="date-picker"
+                                placeholderText="Click to select a date"
                                 />
                             </div>
                         </Form.Group>
@@ -175,8 +140,58 @@ const BudgetAndDate = (props) => {
 
                 </div>
         </div>
-                
-                
+            <div className="budget-form-budget col-md-5">
+               <h3 className="budget-form-label font-color">Budget</h3>
+                    <div className="daily-budget-field d-flex ">
+                        {/* <div className=""> */}
+                            <Form.Check
+                                custom
+                                block
+                                label="Daily"
+                                type="radio"
+                                id={"daily-budget-radio"}
+                                className="radio-budget"
+                                name="dailyBudget"
+                                checked={isDailyBudget}
+                                onChange={(e) => changeToDaily(e)}
+                            />
+                        {/* </div> */}
+                       { isDailyBudget ?  
+                       <div className="input-group mx-3">
+                            <div className="input-group-prepend">
+                                <span className="input-group-text" id="basic-addon1">EUR</span>
+                            </div>
+                            <input type="number" name="daily" value={dailyBudget} onChange={(e) => setDailyAndLifetimeBudget(e)} className="form-control" placeholder="Daily budget" aria-label="budget" aria-describedby="basic-addon1"/>
+                        </div> : null }
+                    </div>
+                    <div className="lifetime-budget-field d-flex">
+                        <div className="">  
+                            <Form.Check
+                                custom
+                                block
+                                label="Lifetime"
+                                type="radio"
+                                id={`lifetime-budget-radio`}
+                                className="radio-budget"
+                                name="lifetimeBudget"
+                                checked={isLifetimeBudget}
+                                onChange={(e) => changeToLifetime(e)}
+                            />
+                        </div>
+                        { isLifetimeBudget ?  
+                        <div className="input-group ml-3">
+                            <div className="input-group-prepend">
+                                <span className="input-group-text" id="basic-addon1">EUR</span>
+                            </div>
+                            <input type="number" name="lifetime" value={lifetimeBudget} onChange={(e) => setDailyAndLifetimeBudget(e)} className="form-control" placeholder="Lifetime budget" aria-label="budget" aria-describedby="basic-addon1"/>
+                        </div> : null }
+                    </div>
+                    <div className="spending">
+                        <p className="gray mt-3">You will spend no more than {isLifetimeBudget ? lifetimeBudget + "$ for the whole period."  : `${(dailyBudget*30).toFixed(2)}$/month.` }</p>
+                    </div>
+                 
+            </div>
+
             </form>
         </div>
     );
