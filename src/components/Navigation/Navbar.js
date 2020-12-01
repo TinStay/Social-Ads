f04343337b477fb45ed78dd4201e6fc8dc159811
Martin Staykov <49690101+TinStay@users.noreply.github.com>
2020-12-01 12:20:33 +0100

    Added closeNavbarWhenClicked functionto all navigations links

--- src/components/Navigation/Navbar.js
+++ src/components/Navigation/Navbar.js
@@ -20,19 +20,44 @@ const Navbar = () =>{
     // console.log(currentUser)
 
     const openSignUpModal = () =>{
+        // Hide navbar in mobile view
+        closeNavbarWhenClicked();
+        
         setIsSignup(true);
         handleShow();
     }
 
     const openLoginModal = () =>{
+        // Hide navbar in mobile view
+        closeNavbarWhenClicked();
+
         setIsSignup(false);
         handleShow();
     }
 
+    const handleSignOut = () => {
+        // Hide navbar in mobile view
+        closeNavbarWhenClicked();
+        
+        // Sign out from firebase auth
+        app.auth().signOut()
+    }
+
+    // Hide collapse when a nav link is clicked in mobile view
+    const closeNavbarWhenClicked = () => {
+
+        var navBar = document.getElementById("navbarNav");
+        var navLink = document.getElementById("navbarBtn");
+        
+        navBar.classList.remove("show");
+        navLink.classList.add("collapsed");
+    }
+
+
     let navbarItems = (
         <ul className="navbar-nav">
             <li className="nav-item">
-                <Link to='/' className="nav-link">How to run ads?</Link>
+                <NavLink to='/' className="nav-link" onClick={() => closeNavbarWhenClicked()}>Contact us</NavLink>
             </li>
             <li className="nav-item">
                 <a onClick={openLoginModal} className="nav-link">Login</a>
@@ -47,16 +72,16 @@ const Navbar = () =>{
         navbarItems = (
             <ul className="navbar-nav">
                 <li className="nav-item">
-                    <NavLink to='/how-does-it-work' className="nav-link" activeClassName='is-active'>How to run ads?</NavLink>
+                    <NavLink to='/ad-manager' className="nav-link" onClick={() => closeNavbarWhenClicked()} activeClassName='is-active'>Ad Manager</NavLink>
                 </li>
                 <li className="nav-item">
-                    <NavLink to='/ad-manager'  className="nav-link" activeClassName='is-active'>Ad manager</NavLink>
+                    <NavLink to='/profile' className="nav-link" onClick={() => closeNavbarWhenClicked()} activeClassName='is-active'>Profile</NavLink>
                 </li>
                 <li className="nav-item">
-                    <NavLink to='/profile' className="nav-link" activeClassName='is-active'>Profile</NavLink>
+                    <NavLink to='/how-does-it-work' className="nav-link" onClick={() => closeNavbarWhenClicked()} activeClassName='is-active'>Contact us</NavLink>
                 </li>
                 <li className="nav-item">
-                    <a onClick={() => app.auth().signOut()} className="nav-link" >Log out</a>
+                    <a onClick={() => handleSignOut()} className="nav-link" >Log out</a>
                 </li>
             </ul>
         )
@@ -83,15 +108,17 @@ const Navbar = () =>{
     return(
         // sticky-navbar class
         <div className="sticky-navbar"> 
-            <nav className="navbar navbar-expand-lg navbar-light ">
-            <NavLink to='/'  className="nav-link"><img src={homePic} className="homeBtn" alt="homePic"/></NavLink>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-                {navbarItems}
-            </div>
-        </nav>
+            <nav className="navbar navbar-expand-lg navbar-light " >
+                <NavLink to='/'  className="nav-link" onClick={() => closeNavbarWhenClicked()}>
+                    <img src={homePic} className="homeBtn" alt="homePic"/>
+                </NavLink>
+                <button className="navbar-toggler" id="navbarBtn" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">   
+                    {navbarItems}
+                </div>
+            </nav>
 
         {authType}
 
