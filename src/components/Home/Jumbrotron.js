ef0964069e59f5118562501ce31d4102e98e52c7
Martin Staykov <49690101+TinStay@users.noreply.github.com>
2021-02-25 14:12:08 +0100

    Removed active and focus outline on buttons

--- src/components/Home/Jumbrotron.js
+++ src/components/Home/Jumbrotron.js
@@ -22,8 +22,8 @@ const Jumbotron = props =>{
     return(
         <div className="jumbotron container text-center">
             <h1>Welcome to AdWeDo</h1>
-            <p>We run and Facebook, Instagram and Google ads for you. Sign up and choose your audience. Lorem ipsum dolor, sit amet consectetur adipisicing elit koasjda.</p>
-            <button onClick={() => setShowSignupForm(true)}  className="btn btn-signup">Sign Up</button>
+            <p className="">We run and Facebook, Instagram and Google ads for you. Sign up, choose your audience and run your ads.</p>
+            <button onClick={() => setShowSignupForm(true)}  className="btn btn-signup ">Sign Up</button>
             <SignupForm show={showSignupForm} handleClose={(e) => setShowSignupForm(false)} changeToLogin={changeToLogin}/>
             <LoginForm show={showLoginForm} handleClose={(e) => setShowLoginForm(false)} changeToSignup={changeToSignup}/>
         </div>
