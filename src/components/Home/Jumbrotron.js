6bbee0a614e837399f8e60614465ad9b37e1ab09
Martin Staykov <49690101+TinStay@users.noreply.github.com>
2021-02-25 13:49:27 +0100

    Added functionality to jumbotron button

--- src/components/Home/Jumbrotron.js
+++ src/components/Home/Jumbrotron.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import app from "../../base";
+import SignupForm from '../Auth/SignupForm'
+import LoginForm from '../Auth/LoginForm'
 
 const Jumbotron = props =>{
+    const [showSignupForm, setShowSignupForm] = useState(false);
+    const [showLoginForm, setShowLoginForm] = useState(false);
 
+    const changeToLogin = (e) => {
+        e.preventDefault()
+        setShowSignupForm(false)
+        setShowLoginForm(true)
+    }
+    
+    const changeToSignup = (e) => {
+        e.preventDefault()
+        setShowSignupForm(true)
+        setShowLoginForm(false)
+    }
 
     return(
         <div className="jumbotron container text-center">
             <h1>Welcome to AdWeDo</h1>
             <p>We run and Facebook, Instagram and Google ads for you. Sign up and choose your audience. Lorem ipsum dolor, sit amet consectetur adipisicing elit koasjda.</p>
-            <button onClick={() => app.auth().signOut()} className="btn btn-signup">Sign Up</button>
+            <button onClick={() => setShowSignupForm(true)}  className="btn btn-signup">Sign Up</button>
+            <SignupForm show={showSignupForm} handleClose={(e) => setShowSignupForm(false)} changeToLogin={changeToLogin}/>
+            <LoginForm show={showLoginForm} handleClose={(e) => setShowLoginForm(false)} changeToSignup={changeToSignup}/>
         </div>
     )
 }
