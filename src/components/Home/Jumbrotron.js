02769f2633d0f0ba76c66b46e63da09de1bf0d45
Martin <marti.staykov@gmail.com>
2021-03-05 18:06:28 +0100

    Change jumbotron button when user is logged in
    Sign up button is changed to a link to Ad Manager if a user is logged in.

--- src/components/Home/Jumbrotron.js
+++ src/components/Home/Jumbrotron.js
@@ -1,33 +1,63 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from "react";
 import app from "../../base";
-import SignupForm from '../Auth/SignupForm'
-import LoginForm from '../Auth/LoginForm'
+import { Link } from 'react-router-dom'
+import { AuthContext } from "../Auth/Auth";
+import SignupForm from "../Auth/SignupForm";
+import LoginForm from "../Auth/LoginForm";
 
-const Jumbotron = props =>{
-    const [showSignupForm, setShowSignupForm] = useState(false);
-    const [showLoginForm, setShowLoginForm] = useState(false);
+const Jumbotron = (props) => {
+  const [showSignupForm, setShowSignupForm] = useState(false);
+  const [showLoginForm, setShowLoginForm] = useState(false);
 
-    const changeToLogin = (e) => {
-        e.preventDefault()
-        setShowSignupForm(false)
-        setShowLoginForm(true)
-    }
-    
-    const changeToSignup = (e) => {
-        e.preventDefault()
-        setShowSignupForm(true)
-        setShowLoginForm(false)
-    }
+  const { currentUser } = useContext(AuthContext);
 
-    return(
-        <div className="jumbotron container text-center">
-            <h1>Welcome to AdWeDo</h1>
-            <p className="">We run Facebook, Instagram and Google ads for you. Sign up, choose your audience and run your ads.</p>
-            <button onClick={() => setShowSignupForm(true)}  className="btn btn-signup ">Sign Up</button>
-            <SignupForm show={showSignupForm} handleClose={(e) => setShowSignupForm(false)} changeToLogin={changeToLogin}/>
-            <LoginForm show={showLoginForm} handleClose={(e) => setShowLoginForm(false)} changeToSignup={changeToSignup}/>
-        </div>
-    )
-}
+  const changeToLogin = (e) => {
+    e.preventDefault();
+    setShowSignupForm(false);
+    setShowLoginForm(true);
+  };
+
+  const changeToSignup = (e) => {
+    e.preventDefault();
+    setShowSignupForm(true);
+    setShowLoginForm(false);
+  };
+
+  return (
+    <div className="jumbotron container text-center">
+      <h1>Welcome to AdWeDo</h1>
+      <p className="">
+        We run Facebook, Instagram and Google ads for you. Sign up, choose your
+        audience and run your ads.
+      </p>
+      {currentUser ? (
+        <Link to="/ad-manager"
+          className="btn btn-signup "
+        >
+          Open Ad Manager
+        </Link>
+      ) : (
+        <>
+          <button
+            onClick={() => setShowSignupForm(true)}
+            className="btn btn-signup "
+          >
+            Sign Up
+          </button>
+          <SignupForm
+            show={showSignupForm}
+            handleClose={(e) => setShowSignupForm(false)}
+            changeToLogin={changeToLogin}
+          />
+          <LoginForm
+            show={showLoginForm}
+            handleClose={(e) => setShowLoginForm(false)}
+            changeToSignup={changeToSignup}
+          />
+        </>
+      )}
+    </div>
+  );
+};
 
 export default Jumbotron;
